5fa7415ab5d4e47c327e75a455fc372bd1cb1e34
joyfulben <benfishernow@gmail.com>
2024-11-08 10:50:53 -0700

    Update logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from "react";
 export const Header = () => {
     return (
     <div className="header-container">
-        <img className="ocm-logo" src="ocmlogo.png"/>
-        <h2>Mapping Occupational Trends</h2>
+        <div className="logo-container">
+            <img className="ocm-logo" src="owlogo.png"/>
+        </div>
+        <h2>Occupational Wages</h2>
     </div>
     )
 }
