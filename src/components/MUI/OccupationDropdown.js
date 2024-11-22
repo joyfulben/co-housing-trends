be1998a698641044b246208affd05a0a8b148698
joyfulben <benfishernow@gmail.com>
2024-11-22 12:37:41 -0700

    Update dropdown to be autocomplete search bar

--- src/components/MUI/OccupationDropdown.js
+++ src/components/MUI/OccupationDropdown.js
@@ -1,14 +1,11 @@
 import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 function OccupationDropdown({ occList, setOccId, occTitle, setOccTitle }) {
     // Log `occList` to debug when it changes
     useEffect(() => {
-        
     }, [occList]);
     // Handle change in selected occupation
     const handleFilterChange = (event) => {
@@ -20,30 +17,13 @@ function OccupationDropdown({ occList, setOccId, occTitle, setOccTitle }) {
 
     // Safely access `occupations` in `occList`
     const occupations = occList?.occupations || [];
-
     return (
         <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <InputLabel id="occupation-selector">Occupation</InputLabel>
-                <Select
-                    labelId="occupation-selector"
-                    id="select-occupation"
-                    value={occTitle || ""}
-                    label="Occupation"
-                    disabled={occupations.length === 0}
-                    onChange={handleFilterChange}
-                >
-                    {occupations.length > 0 ? (
-                        occupations.map((occupation) => (
-                            <MenuItem onClick={()=>setOccTitle(occupation.title)} key={occupation.id} value={occupation.id}>
-                                {occupation.title}
-                            </MenuItem>
-                        ))
-                    ) : (
-                        <MenuItem disabled>No Occupations Loaded</MenuItem>
-                    )}
-                </Select>
-            </FormControl>
+            <Autocomplete
+                disablePortal
+                options={occupations}
+                renderInput={(params)=> <TextField {...params} label="Occupation" />}
+            />
         </Box>
     );
 }
