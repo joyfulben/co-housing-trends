071471681c2514c269de8039dc9c2b46ef17de6a
joyfulben <benfishernow@gmail.com>
2024-11-22 13:32:39 -0700

    Move filter dropdown out of component. Rerender chart when occupation data is updated.

--- src/components/ChartDisplay.js
+++ src/components/ChartDisplay.js
@@ -1,14 +1,11 @@
 import React, { useMemo, useCallback, useEffect } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, Tooltip, Legend, BarElement } from "chart.js";
-import FilterDropdown from './MUI/FilterDropdown';
 
 ChartJS.register(CategoryScale, LinearScale, Tooltip, Legend, BarElement);
 
-function ChartDisplay({ specificOcc, setFilter, filter }) {
-
+function ChartDisplay({ specificOcc }) {
     useEffect(()=>{
-        console.log("SHow me the specific occupation data please: ", specificOcc);
     },[specificOcc])
     // Memoize chart data and options
     const sampleData = useMemo(() => ({
@@ -19,7 +16,7 @@ function ChartDisplay({ specificOcc, setFilter, filter }) {
                 data: specificOcc.wages,
                 showLine: false,
                 fill: false,
-                backgroundColor: ['rgb(0, 219, 76)', 'rgb(0, 224, 81)', /* more colors */],
+                backgroundColor: ['blue', 'green', /* more colors */],
                 borderColor: ['silver'],
                 borderWidth: 1,
                 tooltip: {
@@ -28,10 +25,7 @@ function ChartDisplay({ specificOcc, setFilter, filter }) {
                             let label = context.dataset.label || '';
                             if (label) label += ': ';
                             if (context.parsed.y !== null) {
-                                label += new Intl.NumberFormat('en-US', {
-                                    style: 'currency',
-                                    currency: 'USD',
-                                }).format(context.parsed.y);
+                                label += '$'+context.formattedValue;
                             }
                             return label;
                         },
@@ -39,7 +33,7 @@ function ChartDisplay({ specificOcc, setFilter, filter }) {
                 },
             },
         ],
-    }), []);
+    }), [specificOcc]);
 
     const options = useMemo(() => ({
         responsive: true,
@@ -51,14 +45,8 @@ function ChartDisplay({ specificOcc, setFilter, filter }) {
         // plugins: { tooltip: { backgroundColor: '#ff789a' } },
     }), []);
 
-    // Memoize filter change handler
-    const handleFilterChange = useCallback((newFilter) => setFilter(newFilter), []);
-
     return (
         <div>
-            <div className="filter-btn-container">
-                <FilterDropdown filter={filter} setFilter={handleFilterChange} />
-            </div>
             <div id="graph-container">
                 <Bar options={options} data={sampleData} />
             </div>
