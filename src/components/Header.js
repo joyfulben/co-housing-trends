b0cdd489e68d76a7cc3106b5ca7a4857bcc44bcf
joyfulben <benfishernow@gmail.com>
2024-12-05 15:19:11 -0700

    Update logo and animate header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ export const Header = () => {
     return (
     <div className="header-container">
         <div className="logo-container">
-            <img className="ocm-logo" src="owlogo.png"/>
+            <div className="spinner">
+                <img src="100_dollar_bill.png" alt="100 dollar bill" className="money" />
+            </div>
+            <img className="ocm-logo" src="wage_map_logo.png"/>
         </div>
         <h2>Occupational Wages</h2>
     </div>
