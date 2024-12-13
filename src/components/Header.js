b267c163ce51e5e688d4150e5465835625543974
joyfulben <benfishernow@gmail.com>
2024-12-13 13:14:31 -0700

    Update header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,16 @@ export const Header = () => {
     <div className="header-container">
         <div className="logo-container">
         </div>
-        <h2><img className="header-letter" src="w_logo.png"/>age <img className="header-letter" src="m_logo.png"/>ap</h2>
+        <div className="title-container">
+            <h2>
+                <img className="header-letter" src="w_logo.png"/>
+                age
+            </h2>
+            <h2>
+                <img className="header-letter" src="m_logo.png"/>
+                ap
+            </h2>
+        </div>
     </div>
     )
 }
