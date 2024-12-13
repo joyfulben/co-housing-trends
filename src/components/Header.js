6cb42fad7391570b2b86810061c2cc6c1ec04336
joyfulben <benfishernow@gmail.com>
2024-12-13 13:03:56 -0700

    Add logo letters to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,8 @@ export const Header = () => {
     return (
     <div className="header-container">
         <div className="logo-container">
-            <div className="spinner">
-                <img src="100_dollar_bill.png" alt="100 dollar bill" className="money" />
-            </div>
-            <img className="ocm-logo" src="wage_map_logo.png"/>
         </div>
-        <h2>Wage Map</h2>
+        <h2><img className="header-letter" src="w_logo.png"/>age <img className="header-letter" src="m_logo.png"/>ap</h2>
     </div>
     )
 }
