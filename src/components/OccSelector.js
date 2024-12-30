99cc815a2c2ceedfd79e2070c23efd0ba28570a8
joyfulben <benfishernow@gmail.com>
2024-12-30 13:57:02 -0700

    Remove selected class from previous state

--- src/components/OccSelector.js
+++ src/components/OccSelector.js
@@ -42,7 +42,8 @@ export const OccSelector = () =>{
     },[occId,filter]);
 
     function displaySideChart(e,state) {
-        console.log("Current target: ", e);
+        const previousState = document.querySelector(`[title=${selectedState}]`);
+        previousState.classList.remove("selected");
         setSelectedState(state); // This will still update the state for other parts of the app.
         e.target.classList.add("selected"); //Dynamically update the selected state's class to selected
         // Use the `state` parameter directly
